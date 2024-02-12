1c48eb3080e370673b6c4cd8108d648a30c6f546
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-12 19:26:10 +0100

    nugets added

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -3,8 +3,6 @@
     public class AppUser
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
         public string UserName { get; set; }
 
     }
