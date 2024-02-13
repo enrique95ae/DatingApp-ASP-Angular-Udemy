5debcd74a646df438580771a2992493f51ae057a
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-13 19:30:50 +0100

    Removed unnecessary code

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
+
   imports: [
     BrowserModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
