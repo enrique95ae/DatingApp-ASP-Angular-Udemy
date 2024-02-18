a60d3901e7a47c2b1cb3acf2167ec631d09a39e5
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-19 00:27:54 +0100

    update controllers

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,8 +1,10 @@
-﻿namespace API.Entities
+﻿
+namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
 
         public byte[] PasswordHash { get; set; }
