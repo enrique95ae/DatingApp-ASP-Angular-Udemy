7268c425a352b359d11001fca3a259156520abf1
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-19 21:39:25 +0100

    Authentication

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -40,6 +40,25 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync( x => x.UserName == loginDto.UserName );
+
+            if ( user == null ) return Unauthorized( "User doesn't exist" );
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash( Encoding.UTF8.GetBytes( loginDto.Password ) );
+
+            for( int i = 0; i < computedHash.Length; i++ )
+            {
+                if ( computedHash[i] != user.PasswordHash[i] )
+                    return Unauthorized( "Invalid password" );
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists( string pUsername )
         {
             return await _context.Users.AnyAsync( x => x.UserName == pUsername.ToLower() );
