fa0e1ce571dfdb014ccc1a015a86a9adaa47ae8e
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-20 23:59:34 +0100

    Update Program.cs

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -12,9 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddApplicationServices(builder.Configuration);
-
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
