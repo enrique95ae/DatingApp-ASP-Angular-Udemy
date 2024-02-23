33ad1130d3fe0132e446f22a2b20170877d79158
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-23 22:04:42 +0100

    Added Login form to navComponent

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 
@@ -19,6 +20,7 @@ import { NavComponent } from './nav/nav.component'
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule
   ],
 
   providers: [],
