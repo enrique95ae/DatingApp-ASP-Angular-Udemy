c56ae4c87f8444de32cb901363e04e6dd54c8262
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-23 18:56:29 +0100

    NavBar base bootstrap

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component'
 
 @NgModule({
 
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
 
   imports: [
