1304fde27eb5e0be2369da3956e6c3a6ebed26fa
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-24 23:00:56 +0100

    Added toggle dropdown menu and moved logout option into it

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component'
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
 
@@ -20,7 +22,8 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot(),
   ],
 
   providers: [],
--- ClientUI/ClientUI/src/app/nav/nav.component.ts
+++ ClientUI/ClientUI/src/app/nav/nav.component.ts
@@ -29,5 +29,9 @@ export class NavComponent implements OnInit {
     })
   }
 
+  logout() {
+    this.loggedIn = false;
+  }
+
 }
 
