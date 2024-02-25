5b9502ff0c3941cfb7fc4df92eea4adc4f4aa5bd
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-26 00:51:45 +0100

    updates

--- ClientUI/ClientUI/src/app/_services/account.service.ts
+++ ClientUI/ClientUI/src/app/_services/account.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ export class AccountService {
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((response: any) => {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
         const user = response;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user))
