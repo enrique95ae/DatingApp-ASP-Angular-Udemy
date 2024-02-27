309767db6f350ba9d8a77e84b0ee9a5b2b8b9641
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-02-27 20:47:38 +0100

    more routing

--- ClientUI/ClientUI/src/app/nav/nav.component.ts
+++ ClientUI/ClientUI/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,9 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { 
+
+  }
 
   ngOnInit(): void {
 
@@ -20,17 +23,14 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => {
-        console.log(error);
-      }
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => console.log(error),   
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
