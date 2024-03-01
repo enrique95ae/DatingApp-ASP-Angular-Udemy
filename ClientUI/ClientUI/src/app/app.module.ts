d9117e4ad22cebb92647f36adc8591da39fcd231
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-01 15:46:02 +0100

    Added toastr error popups

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
@@ -37,6 +38,7 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserAnimationsModule,
     FormsModule,
     BsDropdownModule.forRoot(),
+    ToastrModule.forRoot( { positionClass: 'toast-bottom-right' } ), 
   ],
 
   providers: [],
--- ClientUI/ClientUI/src/app/nav/nav.component.ts
+++ ClientUI/ClientUI/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router) { 
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { 
 
   }
 
@@ -24,7 +25,9 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl('/members'),
-      error: error => console.log(error),   
+      error: error => {
+        this.toastr.error(error.error);
+      },   
     })
   }
 
