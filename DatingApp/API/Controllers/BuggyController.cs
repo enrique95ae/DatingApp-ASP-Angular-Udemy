516111c05af11983c21565bc6da851683ea6af4e
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-04 21:21:18 +0100

    Errors controller

---
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+
+        public BuggyController(DataContext dataContext )
+        {
+            _dataContext = dataContext;
+        }
+
+        [Authorize]
+        [HttpGet( "auth" )]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet( "not-found" )]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _dataContext.Users.Find(-1);
+
+            if ( thing == null ) return NotFound();
+
+            return thing;
+        }
+
+        [HttpGet( "server-error" )]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _dataContext.Users.Find( -1 );
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+
+        [HttpGet( "bad-request" )]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest( "This was not a good request" );
+        }
+    }
+}
