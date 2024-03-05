baf85b9785be952261b8eb07a506c4b2c54c15b1
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-05 14:34:33 +0100

    error components

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { SharedModule } from './_modules/shared.module';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
+    TestErrorComponent,
 
   ],
 
---
+++ ClientUI/ClientUI/src/app/errors/test-error/test-error.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/'
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  get404error(){
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400error(){
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500error(){
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401error(){
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError(){
+    this.http.get(this.baseUrl + 'account/register').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+}
