5b7b03dc97fd210c18b9a44f2db25ef590099cad
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-06 18:27:50 +0100

    Validation error

--- ClientUI/ClientUI/src/app/errors/test-error/test-error.component.ts
+++ ClientUI/ClientUI/src/app/errors/test-error/test-error.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.css']
 })
 export class TestErrorComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) {
 
@@ -48,7 +49,10 @@ export class TestErrorComponent implements OnInit {
   get400ValidationError(){
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        this.validationErrors = error;
+      }
     })
   }
 }
