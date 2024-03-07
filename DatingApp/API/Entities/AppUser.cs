d121115226989f4d914413ecbf88d8e4ae2a9218
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-07 21:37:29 +0100

    Updated models and migrations for database

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,13 +1,31 @@
 ﻿
+using API.Extensions;
+using System.Linq.Expressions;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Countrye { get; set; }
+        public List<Photo> Photos { get; set; } = new List<Photo>();
+            
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
---
+++ DatingApp/API/Entities/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("Photos")] //This makes entity framework to create the DB with this name instead of the class name
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId {  get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+    }
+
+}
