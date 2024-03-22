d10692de115eeac1c489504c539dd77491b87cd4
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-22 12:47:40 +0100

    adding user main photo to nav bar

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -42,16 +42,19 @@ namespace API.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            return new UserDto{
+            return new UserDto {
                 Username = pRegisterDto.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken( user ),
+                PhotoUrl = user.Photos.FirstOrDefault( x => x.IsMain )?.Url
             };
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.FirstOrDefaultAsync( x => x.UserName == loginDto.UserName );
+            var user = await _context.Users
+                .Include( p => p.Photos )
+                .SingleOrDefaultAsync( x => x.UserName == loginDto.UserName );
 
             if ( user == null ) return Unauthorized( "User doesn't exist" );
 
@@ -68,7 +71,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = loginDto.UserName,
-                Token = _tokenService.CreateToken( user )
+                Token = _tokenService.CreateToken( user ),
+                PhotoUrl = user.Photos.FirstOrDefault( x => x.IsMain )?.Url
             };
         }
 
