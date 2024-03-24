38ea9c74bd66e6c222cd38ac0def3276b8780926
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-24 19:21:43 +0100

    reactive forms

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -22,6 +22,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 
 @NgModule({
@@ -40,6 +41,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
 
   ],
 
@@ -49,6 +51,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ],
 
--- ClientUI/ClientUI/src/app/register/register.component.ts
+++ ClientUI/ClientUI/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
-import { outputAst } from '@angular/compiler';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +11,46 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
 
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) {
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) {
 
   }
 
   ngOnInit(): void {
-    
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender:  ['male'],
+      username:  ['', Validators.required],
+      knownAs:  ['', Validators.required],
+      dateOfBirth:  ['', Validators.required],
+      city:  ['', Validators.required],
+      country:  ['', Validators.required],
+      password:  ['', [Validators.required, 
+                      Validators.minLength(4), 
+                      Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required,
+                                            this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register() {
+
+    console.log(this.registerForm?.value);
+    /*
     this.accountService.register(this.model).subscribe({
       next: () => {
         this.cancel();
@@ -31,6 +60,7 @@ export class RegisterComponent implements OnInit{
         this.toastr.error(error.error);
       }
     });
+    */
   }
 
   cancel() {
