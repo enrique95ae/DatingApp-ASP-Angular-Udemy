8b887f0dc8e7058dd174e79eec6849789716676c
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-03-24 19:52:56 +0100

    reusable date input

--- ClientUI/ClientUI/src/app/app.module.ts
+++ ClientUI/ClientUI/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 
 
 @NgModule({
@@ -42,6 +43,7 @@ import { TextInputComponent } from './_forms/text-input/text-input.component';
     MemberEditComponent,
     PhotoEditorComponent,
     TextInputComponent,
+    DatePickerComponent,
 
   ],
 
--- ClientUI/ClientUI/src/app/register/register.component.ts
+++ ClientUI/ClientUI/src/app/register/register.component.ts
@@ -12,6 +12,7 @@ export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
   registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
 
 
   constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) {
@@ -20,6 +21,7 @@ export class RegisterComponent implements OnInit{
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
   }
 
   initializeForm() {
